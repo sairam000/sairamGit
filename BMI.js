1996cb2e08b10769951d7cf2d333592b68442d62
sairam000 <62096416+sairam000@users.noreply.github.com>
2022-03-22 10:00:51 +0530

    update the code with the input

--- BMI.js
+++ BMI.js
@@ -1,5 +1,8 @@
 
-var text = '[{ "gender":"male", "heightCm":"175", "weightKg":"30"},{ "gender":"male", "heightCm":"175", "weightKg":"90"}]';
+var text = '[{"Gender": "Male", "HeightCm": 171, "WeightKg": 96 }, {"Gender": "Male", "HeightCm": 161,
+"WeightKg":85 }, { "Gender": "Male", "HeightCm": 180, "WeightKg": 77 }, { "Gender": "Female",
+"HeightCm": 166,"WeightKg": 62}, {"Gender": "Female", "HeightCm": 150, "WeightKg": 70},
+{"Gender": "Female","HeightCm": 167, "WeightKg": 82}]';
 var obj = JSON.parse(text);
 count=0;
 for(let x in obj)
